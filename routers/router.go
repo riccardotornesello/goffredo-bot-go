123075bc11787d277fba8f874a2dfa1734798d74
Riccardo Tornesello <riccardo.tornesello@gmail.com>
2022-02-20 10:24:42 +0000

    serve static

--- routers/router.go
+++ routers/router.go
@@ -7,6 +7,7 @@ import (
 )
 
 func init() {
+	beego.SetStaticPath("/static","static")
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/login", &controllers.LoginController{})
 	beego.Router("/logout", &controllers.LogoutController{})
